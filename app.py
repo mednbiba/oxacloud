fba5d1c747bfb8b7f1423518632900a8e632f844
nbiba <mednbiba@gmail.com>
2022-08-09 11:22:41 +0100

    basic script v0.2

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def getVMs(sessionID):
 
 #Create VM Function Basic
 #TO-DO :
-#Change function to take JSON file as a parameter
+#Change function to parse JSON file as a parameter
 #Modify API Call in Flask Server
 def createVM(sessionID,NameVM):
     uricreatevm='https://102.164.112.135/api/vcenter/vm'
@@ -67,16 +67,16 @@ def createVM(sessionID,NameVM):
     response = requests.post('https://102.164.112.135/api/vcenter/vm', headers=headers, json=json_data,verify=False).content
     vmcreate=json.loads(response)
     return vmcreate
-
+#Takes NameVM = VM ID i.e : "vm-1046"
 def deleteVM(sessionID,NameVM):
     uridelete='https://102.164.112.135/api/vcenter/vm/'+NameVM
     headers = {'vmware-api-session-id': ''+sessionID+''}
     delete = requests.delete(uridelete,verify=False,headers=headers).content
-    print(sessionID)
+    print(delete)
 
 #how to run flask application/api:
-#pip install Flask
-#flask run
+#>pip install Flask
+#>flask run
 
 
 app = Flask(__name__)
@@ -96,7 +96,11 @@ def hosts():
 def vm():
     name=request.args.get('Name')
     return jsonify(createVM(sessionID,name))
-
+#Add query parameter in request url i.e : http://127.0.0.1:5000/delete?ID=Testing
+@app.route('/delete', methods = ['DELETE'])
+def delete():
+    name=request.args.get('ID')
+    return jsonify(deleteVM(sessionID,name))
 
 
 
