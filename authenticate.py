afa41c181272326e11439602e7d4c85c26c50e17
nbiba <mednbiba@gmail.com>
2022-08-10 11:29:07 +0100

    added authentication

---
+++ authenticate.py
@@ -0,0 +1,11 @@
+from venv import create
+import requests
+import json
+import urllib3
+
+def createSession():
+    uri = 'https://102.164.112.135/api/session'
+    sessionRequest = requests.post(uri,verify=False, auth=('stage@vsphere.local', 'djZ82L-28+qZKJJ'))
+    return(str(sessionRequest.text).strip('\"'))
+
+print(createSession())
