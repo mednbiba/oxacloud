91330b44c8e729a0cc5f3a1e41d2d3823e43a1e4
Mohamed N <93776416+mednbiba@users.noreply.github.com>
2023-12-12 20:59:10 +0100

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -4,7 +4,7 @@ import json
 import urllib3
 
 def createSession():
-    uri = 'https://102.164.112.135/api/session'
-    sessionRequest = requests.post(uri,verify=False, auth=('stage@vsphere.local', 'djZ82L-28+qZKJJ'))
+    uri = 'https://102.165.112.135/api/session'
+    sessionRequest = requests.post(uri,verify=False, auth=('xd@vsphere.local', 'password'))
     return(str(sessionRequest.text).strip('\"'))
 
